356d58d02668bfb20ea7dfcecca34d0b181a5739
Tohpzy <46164787+Tohpzy@users.noreply.github.com>
2022-07-05 09:46:29 +0100

    Add files via upload

---
+++ blood project/js/app.js
@@ -0,0 +1,8 @@
+// hamburger section
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('nav');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active')
+}
